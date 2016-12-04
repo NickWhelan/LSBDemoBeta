590b362a4c56c9e1fb5b28e99d88ced5e181007e
Nick Whelan <nickwhelan5@gmail.com>
2016-12-04 13:29:05 -0500

    Making the Project

---
+++ LSP/Assets/Scripts/Roomba/MoveRoomba.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRoomba : MonoBehaviour
+{
+    public bool Move = false;
+    public bool Forward, Left, Right, Back;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Move)
+        {
+            if (Forward)
+            {
+                transform.position += new Vector3(0, 0, 1);
+            }
+            if (Left)
+            {
+                transform.Rotate(0, -1, 0);
+            }
+            if (Right)
+            {
+                transform.Rotate(0, 1, 0);
+            }
+            if (Back)
+            {
+                transform.position += new Vector3(0, 0, -1);
+            }
+        }
+    }
+}
