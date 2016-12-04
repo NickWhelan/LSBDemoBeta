f61948dbdafae8be02d620b08ea5634d6ef9ef82
Raymond Micheau <raymicheau@yahoo.ca>
2016-12-04 14:31:52 -0500

    Timer Added
    Added a timer and updated to Unity 5.5.0.f3

---
+++ LSP/Assets/Scripts/Timer.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+
+    float seconds = 0;
+
+    [Range(1, 10), SerializeField]
+    int minutes = 1;
+    float milliseconds = 0;
+
+    Text timerText;
+
+	// Use this for initialization
+	void Start () {
+        timerText = GetComponentInChildren<Text>();
+        timerText.text = "00 : 00 : 00 ";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (milliseconds <= 0)
+        {
+            if (seconds <= 0 && minutes <= 0)
+            {
+                Debug.Log("Game should end here");
+            }
+            if (seconds <= 0)
+            {
+                minutes--;
+                seconds = 59;
+            }
+            else if (seconds >= 0)
+            {
+                seconds--;
+            }
+            milliseconds = 100;
+        }
+
+
+        milliseconds -= Time.deltaTime * 100;
+        timerText.text = string.Format("{0} : {1} : {2}", minutes, seconds, (int)milliseconds);
+
+
+    }
+}
